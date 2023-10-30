ba7d29ec52b076181f56329586578b7e498a5be6
César Centurión <cesarcent17@outlook.com>
2023-10-29 22:57:38 -0500

    Added button to download cv

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   imageUrl: string = '../../../assets/jpg/photo-cent-1.jpg';
+
+  constructor(private http: HttpClient) {}
+  descargarCV() {
+    const pdfUrl = '../../../assets/pdf/cv-10-2023.pdf';
+
+    this.http.get(pdfUrl, { responseType: 'arraybuffer' }).subscribe(data => {
+      const blob = new Blob([data], { type: 'application/pdf' });
+
+      const blobUrl = window.URL.createObjectURL(blob);
+
+      const a = document.createElement('a');
+      a.href = blobUrl;
+      a.download = 'cv-10-2023.pdf';
+      a.click();
+
+      window.URL.revokeObjectURL(blobUrl);
+      a.remove();
+    });
+  }
 }
